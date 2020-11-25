18f5dc5ba6c42eac490e4171ab602a3db995e027
Jonathan Bispo <jonathan26062001@hotmail.com>
2020-11-25 16:07:12 -0300

    arrumando quebra de linha com \r\n

--- pass2/teste.py
+++ pass2/teste.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     git_username, git_access_token = credentials.load("tokens.txt")
     switch_account = 0
 
-    repo = request_manager.request("https://api.github.com/repos/apache/commons-io", git_username, git_access_token, switch_account)
+    repo, switch_account = request_manager.request("https://api.github.com/repos/wala/WALA/commits?per_page=1", git_username, git_access_token, switch_account)
+    repo_last_commit_message = repo[0]["commit"]["message"]
     if repo == "404: Not Found":
-        print("repo vazio")    
-    print(repo)
+        print("repo vazio")
+
+    print( )
